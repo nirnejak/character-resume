355c6386db686e4149859438d5f8dcd1d3579848
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-01-26 21:39:45 +0530

    Characters Services Updated

--- src/services/characters.js
+++ src/services/characters.js
@@ -1,6 +1,9 @@
 export const fetchAllCharacters = async (params) => {
   try {
-    const response = await fetch("https://rickandmortyapi.com/api/character")
+    const response = await fetch(
+      "https://rickandmortyapi.com/api/character" +
+        new URLSearchParams(params.params)
+    )
     const data = await response.json()
     return data
   } catch (e) {
@@ -22,7 +25,14 @@ export const fetchCharacter = async (params) => {
 
 export const createCharacter = async (params) => {
   try {
-    const response = await fetch("https://rickandmortyapi.com/api/character")
+    const response = await fetch("https://rickandmortyapi.com/api/character", {
+      body: params.body,
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      method: "POST",
+    })
     const data = await response.json()
     return data
   } catch (e) {
@@ -33,7 +43,15 @@ export const createCharacter = async (params) => {
 export const updateCharacter = async (params) => {
   try {
     const response = await fetch(
-      `https://rickandmortyapi.com/api/character/${params.id}`
+      `https://rickandmortyapi.com/api/character/${params.id}`,
+      {
+        body: params.body,
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        method: "PUT",
+      }
     )
     const data = await response.json()
     return data
@@ -45,7 +63,10 @@ export const updateCharacter = async (params) => {
 export const deleteCharacter = async (params) => {
   try {
     const response = await fetch(
-      `https://rickandmortyapi.com/api/character/${params.id}`
+      `https://rickandmortyapi.com/api/character/${params.id}`,
+      {
+        method: "DELETE",
+      }
     )
     const data = await response.json()
     return data
