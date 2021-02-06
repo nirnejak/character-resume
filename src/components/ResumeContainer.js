3d58d4964e977c04192206b3bfbac08561819129
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-02-07 03:02:59 +0530

    Resume Container: Prev/Next Buttons Added

--- src/components/ResumeContainer.js
+++ src/components/ResumeContainer.js
@@ -1,11 +1,26 @@
+import { useParams, Link } from "react-router-dom"
+
 import PropTypes from "prop-types"
 
 import "./ResumeContainer.scss"
 
 const ResumeContainer = ({ children }) => {
+  const { id } = useParams()
+
+  const prevResume = parseInt(id) - 1
+  const nextResume = parseInt(id) + 1
+
   return (
     <section className="resume-container">
-      <div className="resume">{children}</div>
+      <div className="resume">
+        <div className="link">
+          {prevResume > 0 && <Link to={`/resume/${prevResume}`}>{"<<"}</Link>}
+        </div>
+        <div className="card">{children}</div>
+        <div className="link">
+          <Link to={`/resume/${nextResume}`}>{">>"}</Link>
+        </div>
+      </div>
     </section>
   )
 }
